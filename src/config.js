a454573502d84ae9b902bce076eaf354bd07f663
Ryan Mark Hiluano <hryanmark@gmail.com>
2023-09-20 17:11:29 +0900

    Axios implementation (GET request)

--- src/config.js
+++ src/config.js
@@ -1,1 +1,2 @@
 export const DRAWER_WIDTH = 240;
+export const API_URL = 'http://localhost:8080/api/v1'
--- src/page/CategoryPage.jsx
+++ src/page/CategoryPage.jsx
@@ -15,41 +15,38 @@ import Fab from '@mui/material/Fab';
 import SideNavigationBar from '../component/SideNavigationBar';
 import Header from '../component/Header';
 import BreadCrumbs from '../component/BreadCrumbs';
-
-const columns = [
-    { field: 'id', headerName: 'ID', width: 70 },
-    { field: 'firstName', headerName: 'First name', width: 130 },
-    { field: 'lastName', headerName: 'Last name', width: 130 },
-    {
-      field: 'age',
-      headerName: 'Age',
-      type: 'number',
-      width: 90,
-    },
-    {
-      field: 'fullName',
-      headerName: 'Full name',
-      description: 'This column has a value getter and is not sortable.',
-      sortable: false,
-      width: 160,
-      valueGetter: (params) =>
-        `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-    },
-  ];
-  
-  const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-  ];
+import { useState, useEffect } from 'react';
+import { API_URL } from '../config';
+import axios from 'axios';
 
 export default function CategoryPage() {
+  
+  const [data, setData] = useState([]);
+  const [columns, setColumns] = useState([]);
+
+  useEffect(() => {
+    const apiUrl = API_URL + '/category';
+
+    axios.get(apiUrl)
+      .then(response => {
+        setData(response.data);
+
+        if (response.data.length > 0){
+          const keys = Object.keys(response.data[0]);
+          const generatedColumns = keys.map((key) => ({
+            field: key,
+            headerName: key.toUpperCase(),
+            width: 150,
+          }));
+
+          setColumns(generatedColumns);
+        }
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  }, []);
+
   return (
     <Box sx={{ display: "flex" }}>
       <CssBaseline />
@@ -129,7 +126,7 @@ export default function CategoryPage() {
         <Card sx={{ marginTop: "15px" }}>
           <div style={{ height: 400, width: "100%" }}>
             <DataGrid
-              rows={rows}
+              rows={data}
               columns={columns}
               initialState={{
                 pagination: {
